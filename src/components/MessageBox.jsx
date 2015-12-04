de2671d3d3dfa5d5c69a74959ca6fc381d4f82ce
Joshua Chan <joshua@Joshuas-MBP.home>
2015-12-03 23:12:06 -0600

    Using Firebase

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import mui from 'material-ui';
+import trim from 'trim';
+import Firebase from 'firebase';
 
 var {Card} = mui;
 
 class MessageBox extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            message: ''
+        }
+        
+        this.firebaseRef = new Firebase("https://fc-reactfluxwebpack.firebaseio.com/messages");
+    }
+    
+    onChange(evt){
+        this.setState({
+            message: evt.target.value
+        });
+    }
+    
+    onKeyUp(evt){
+        if(evt.keyCode === 13 && trim(evt.target.value) != ''){
+            evt.preventDefault();
+            
+            this.firebaseRef.push({
+                message: this.state.message
+            })
+            
+            this.setState({
+                message: ''
+            })
+        }
     }
 
     render(){
@@ -15,7 +43,11 @@ class MessageBox extends React.Component {
                 margin: '30px auto 30px',
                 padding: 30
             }}>
-				<textarea style={{
+				<textarea 
+                  value={this.state.message}
+                  onChange={this.onChange.bind(this)}
+                  onKeyUp={this.onKeyUp.bind(this)}
+                  style={{
                     width: '100%',
                     borderColor: '#D0D0D0',
                     resize: 'none',
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx';
 import mui from 'material-ui';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 var {Card, List} = mui;
 
@@ -8,17 +10,37 @@ class MessageList extends React.Component {
     constructor() {
         super();
         this.state = {
-            messages: [
-                'hi there how are you?',
-                'I am fine, and you?'
-            ]
+            messages: {}
         }
+        
+        this.firebaseRef = new Firebase("https://fc-reactfluxwebpack.firebaseio.com/messages");
+    }
+    
+    componentDidMount(){
+        
+        this.firebaseRef.on("child_added", (msg) => {
+            
+            if(this.state.messages[msg.key()]) return;
+            
+            let msgVal = msg.val();
+            msgVal.key = msg.key();
+            
+            this.state.messages[msgVal.key] = msgVal;
+            this.setState({messages: this.state.messages})
+        })
+        
+        this.firebaseRef.on("child_removed", (msg) => {
+            
+            var key = msg.key();
+            delete this.state.messages[key];
+            this.setState({ messages: this.state.messages });
+        })
     }
 
     render(){
-        var messageNodes = this.state.messages.map((message) => {
+        var messageNodes = _.values(this.state.messages).map((message) => {
             return (
-                <Message message={message} />
+                <Message message={message.message} />
             );
         });
 
